bb730f3f58f9e42e7a1afd746e21db5be981e34c
liweidong <liweidong@huobi.com>
2021-08-30 10:45:10 +0800

    monitor_price plugin

--- plugins/monitor_price_plugin/include/hb/monitor_price_plugin/monitor_price_error.h
+++ plugins/monitor_price_plugin/include/hb/monitor_price_plugin/monitor_price_error.h
@@ -5,9 +5,9 @@
 namespace hb{ namespace plugin {
     using namespace hb::error;
 
-    class monitor_price_exception : public hb_exception<monitor_price_exception> {
+    class monitor_price_exception : public hb_exception<monitor_price_exception, SN("monitorprice")> {
     public:
-        monitor_price_exception():hb_exception<monitor_price_exception>(monitor_price_plugin_no, get_title(monitor_price_plugin_no)) {
+        monitor_price_exception():hb_exception<monitor_price_exception, SN("monitorprice")>() {
 
         }
     };
